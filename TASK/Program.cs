176af4c68781203e67d2e252fec6c8e9845277bf
dolja <hpuser230820@oulook.com>
2023-11-23 12:36:33 +0300

    Added solution

---
+++ TASK/Program.cs
@@ -0,0 +1,37 @@
+﻿string [] stringArray = new string[5] {"аб", "125", "hello", "world", "123654"};
+
+string [] SecondArray(string [] stringArray)
+{
+  int count = 0;  
+  for (int i = 0; i<stringArray.Length; i++)
+  {
+    if(stringArray[i].Length <=3)
+    count++;
+  }
+  string [] rez = new string [count];
+  int j = 0;
+  for (int i = 0; i<stringArray.Length; i++)
+  {
+    if(stringArray[i].Length <=3)
+    {
+        rez[j] = stringArray[i];
+        j++;
+    }
+  }
+  return rez;
+}
+
+void PrintA(string [] stringArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+    if(i < stringArray.Length - 1) Console.Write($"“{stringArray[i]}”, ");
+    else Console.Write($"“{stringArray[i]}”");
+    }
+     Console.Write("]");   
+}
+      
+PrintA(stringArray);
+Console.Write(" → ");
+PrintA(SecondArray(stringArray));
